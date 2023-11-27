291e158dab426b53c7b83b1375ec849848d8231f
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-27 16:14:41 +0500

    .

--- FoodAPI/Models/Food.cs
+++ FoodAPI/Models/Food.cs
@@ -5,7 +5,7 @@ namespace FoodAPI.Models
     public class Food
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } 
         public double Price { get; set; }
         public string Description { get; set; }
         public int CategoryId { get; set; }
